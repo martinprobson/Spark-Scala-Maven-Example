8454115fc08d32ec9a6c13b8fb0307cf50c2ad87
Martin Robson <martinprobson@gmail.com>
2020-05-22 16:25:58 +0100

    Add scala style plugin

--- src/main/scala/net/martinprobson/spark/Run.scala
+++ src/main/scala/net/martinprobson/spark/Run.scala
@@ -1,3 +1,18 @@
+// Copyright (C) 2011-2020 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package net.martinprobson.spark
 
 import grizzled.slf4j.Logging
@@ -18,7 +33,7 @@ object Run extends SparkEnvironment with Logging {
         match { case 0 => (i,i)
         case _ => (2,i)}
       }.reduceByKey(_+_).toDS
-    val sparkEnv = org.apache.spark.SparkEnv.get
+    //val sparkEnv = org.apache.spark.SparkEnv.get
     println(ds.count)
     ds.show(10)
     //val emps = spark.sqlContext.emptyDataFrame
--- src/main/scala/net/martinprobson/spark/SparkEnvironment.scala
+++ src/main/scala/net/martinprobson/spark/SparkEnvironment.scala
@@ -1,3 +1,18 @@
+// Copyright (C) 2011-2020 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package net.martinprobson.spark
 
 import com.typesafe.config.ConfigFactory
@@ -44,8 +59,8 @@ trait SparkEnvironment {
   }
 
   /*
-	* Dump spark configuration for the current spark session.
-	*/
+   * Dump spark configuration for the current spark session.
+   */
   private[spark] def getAllConf: String = {
     getSession.conf.getAll.map { case(k,v) => "Key: [%s] Value: [%s]" format (k,v)} mkString("","\n","\n")
   }
